45147d82ef37d37c46fc4eab0c728322ccf2c931
AlanAbelPereyra <alanabelpereyra322@gmail.com>
2024-03-05 19:32:49 -0300

    card-target

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -11,9 +11,20 @@ export default function contact() {
         >
           Contact
         </h1>
-        <div className="flex items-center justify-center mb-10">
-          <div className="items-center w-1/3 h-full justify-center profile-card  rounded-md shadow-xl overflow-hidden z-[100] relative cursor-pointer snap-start shrink-0 bg-white flex flex-col gap-3 transition-all duration-300 group">
-            <div className=" w-full pt-5 flex items-center justify-center flex-col gap-1">
+        <div className=" flex justify-center items-center">
+          <Image
+            src={
+              "https://res.cloudinary.com/dn5ltihzv/image/upload/v1709677824/imagenes/cardContact.svg"
+            }
+            width={800}
+            height={500}
+            alt="imageprofile-contact"
+            className="object-cover mb-20"
+          />
+        </div>
+        {/* <div className="flex right-0  items-center justify-center mb-10">
+          <div className=" w-1/2 mr-20 items-center h-full justify-center profile-card  rounded-md shadow-xl overflow-hidden z-[100] relative cursor-pointer snap-start shrink-0 bg-white flex flex-col gap-3 transition-all duration-300 group">
+            <div className="w-5/6 pt-5 flex items-center justify-center flex-col gap-1">
               <div className="img_container w-full flex items-center justify-center relative z-40 after:absolute after:h-[6px] after:w-full after:bg-[#58b0e0] after:top-4 after:group-hover:size-[1%] after:delay-300 after:group-hover:delay-0 after:group-hover:transition-all after:group-hover:duration-300 after:transition-all after:duration-300 before:absolute before:h-[6px] before:w-full before:bg-[#58b0e0] before:bottom-4 before:group-hover:size-[1%] before:delay-300 before:group-hover:delay-0 before:group-hover:transition-all before:group-hover:duration-300 before:transition-all before:duration-300">
                 <div className="">
                   <Image
@@ -37,7 +48,7 @@ export default function contact() {
                 full stack developer
               </p>
             </div>
-            <div className="w-full items-center justify-center flex">
+            <div className="px-4 w-full items-center justify-center flex">
               <ul className="flex  flex-col items-start gap-2 has-[:last]:border-b-0 *:inline-flex *:gap-2 *:items-center *:justify-center *:border-b-[1.5px] *:border-b-stone-700 *:border-dotted *:text-xs *:font-semibold *:text-[#434955] pb-3">
                 <li className="">
                   <svg
@@ -51,7 +62,7 @@ export default function contact() {
                     <path d="M0 0h24v24H0V0z" fill="none"></path>
                     <path d="M19.23 15.26l-2.54-.29c-.61-.07-1.21.14-1.64.57l-1.84 1.84c-2.83-1.44-5.15-3.75-6.59-6.59l1.85-1.85c.43-.43.64-1.03.57-1.64l-.29-2.52c-.12-1.01-.97-1.77-1.99-1.77H5.03c-1.13 0-2.07.94-2 2.07.53 8.54 7.36 15.36 15.89 15.89 1.13.07 2.07-.87 2.07-2v-1.73c.01-1.01-.75-1.86-1.76-1.98z"></path>
                   </svg>
-                  <p className="text-[18px]">+15 113085-5123</p>
+                  <p className="text-[18px] ">+15 113085-5123</p>
                 </li>
                 <li>
                   <svg
@@ -111,7 +122,7 @@ export default function contact() {
             </div>
             <hr className="w-full mb-5 group-hover:h-5 h-3 bg-[#22d3ee] group-hover:transition-all group-hover:duration-300 transition-all duration-300" />
           </div>
-        </div>
+        </div> */}
       </div>
       <Footer />
     </div>
