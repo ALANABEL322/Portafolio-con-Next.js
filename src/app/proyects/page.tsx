2fb67999ead246507e21456e38b182a9cdc7b95a
AlanAbelPereyra <alanabelpereyra322@gmail.com>
2024-03-06 16:02:20 -0300

    fix card project

--- src/app/proyects/page.tsx
+++ src/app/proyects/page.tsx
@@ -19,7 +19,7 @@ export default function Proyectos() {
       <div className="w-screen max-h-full flex flex-col items-center justify-center">
         <div className="container">
           <h1
-            className={`${styles.projects} text-slate-50 md:mb-[200px] mb-[80px] text-center xs:text-[30px] sm:text-[80px] md:text-[120px] lg:text-[180px] xl:text-[240px]`}
+            className={`${styles.projects} text-slate-50 md:mb-[200px] mb-[80px] text-center text-8xl sm:text-[80px] md:text-[120px] lg:text-[180px] xl:text-[240px]`}
           >
             Projects
           </h1>
@@ -51,7 +51,7 @@ export default function Proyectos() {
             <p className="text-slate-50 xs:text-2xl sm:text-3xl md:text-3xl lg:text-3xl xl:text-4xl mb-3 mr-10">
               ClinicalTechsrl
             </p>
-            <div className="p-5  text-slate-50 xs:text-1xl sm:text-2xl md:text-2xl lg:text-2xl xl:text-2xl mr-10 ml-2 border rounded-md border-zinc-400 bg-gray-900">
+            <div className="p-5  text-slate-50 xs:text-1xl sm:text-2xl md:text-2xl lg:text-2xl xl:text-2xl mr-5 ml-2 border rounded-md border-zinc-400 bg-gray-900">
               As a developer on the ClinicalTech team, I had the opportunity to
               contribute to both the frontend and backend development of this
               exciting ecommerce platform specializing in medical supplies.
