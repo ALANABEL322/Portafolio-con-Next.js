8ef1d4eb420e000be0127d1155ae341ade6f71d5
AlanAbelPereyra <alanabelpereyra322@gmail.com>
2024-03-08 21:49:44 -0300

    styles-hamburguer-bottom

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -8,21 +8,19 @@ export default function NavBar() {
   const [navbar, setNavbar] = useState(false);
   return (
     <div>
-      <nav className="w-full fixed top-0 left-0 right-0 z-50">
+      <nav className="w-full z-[100] fixed top-0 left-0 right-0">
         <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
           <div>
             <div className="flex items-center justify-between py-3 md:py-5 md:block">
-              {/* LOGO */}
               <Link className={`${styles.logo}`} href="/">
                 <Image
                   src="https://res.cloudinary.com/dn5ltihzv/image/upload/v1709754260/imagenes/logo%20SVG.svg"
                   alt="logo"
-                  width={100}
+                  width={80}
                   height={100}
                   className="object-cover"
                 />
               </Link>
-              {/* HAMBURGER BUTTON FOR MOBILE */}
               <div className="md:hidden">
                 <button
                   className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
@@ -50,21 +48,21 @@ export default function NavBar() {
           </div>
           <div>
             <div
-              className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+              className={` flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
                 navbar ? "p-12 md:p-0 block" : "hidden"
               } ${styles.links}`}
             >
-              <ul className="h-screen md:h-auto items-center justify-center md:flex z-[1500]">
-                <li className="pb-6 text-xl text-white py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-purple-900  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+              <ul className=" h-screen md:h-auto items-center justify-center md:flex z-[1500]">
+                <li className=" rounded-t-lg pb-6 text-xl text-white py-2 md:px-6 text-center border-b-2 md:border-b-0  bg-slate-950/50 to-transparent md:bg-transparent">
                   <Link
                     href="/about"
-                    className={`${styles.enlaces} brightness-200 mr-2`}
+                    className={`${styles.enlaces}  brightness-200 mr-2`}
                     onClick={() => setNavbar(!navbar)}
                   >
                     About
                   </Link>
                 </li>
-                <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                <li className="rounded-t-lg my-2 pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  bg-slate-950/50 to-transparent md:bg-transparent">
                   <Link
                     href="/skills"
                     className={`${styles.enlaces} brightness-200 mr-2`}
@@ -73,7 +71,7 @@ export default function NavBar() {
                     Skills
                   </Link>
                 </li>
-                <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                <li className="rounded-t-lg pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  bg-slate-950/50 to-transparent md:bg-transparent">
                   <Link
                     href="/proyects"
                     className={`${styles.enlaces} brightness-200 mr-2`}
@@ -82,10 +80,10 @@ export default function NavBar() {
                     Projects
                   </Link>
                 </li>
-                <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                <li className="rounded-t-lg mt-2 pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  bg-slate-950/50 to-transparent md:bg-transparent">
                   <Link
                     href="/contact"
-                    className={`${styles.enlaces} brightness-200 mr-2`}
+                    className={`${styles.enlaces} brightness-200 mr-2 `}
                     onClick={() => setNavbar(!navbar)}
                   >
                     Contact
@@ -99,5 +97,3 @@ export default function NavBar() {
     </div>
   );
 }
-
-
